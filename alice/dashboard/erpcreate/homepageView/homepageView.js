e66c12bec5d293329dd7f9de58ccc06054e9ba2c
Inspire <nhonha@gmail.com>
2020-12-22 14:02:00 +0800

    fix startup a1

--- alice/dashboard/erpcreate/homepageView/homepageView.js
+++ alice/dashboard/erpcreate/homepageView/homepageView.js
@@ -11,7 +11,7 @@ const fs = require('fs-extra')
 
 const {aliceAsyncMiddleware} = require('@part/alice')
 const {pAdminSuper} = require('@part/auth')
-const {dbFindOneQuery, dbFindOneUpdate, dbFindAllArray, dbFindOneDelete} = require('@part/db')
+const {dbFindOneQuery, dbFindOneUpdate, dbFindAllArray, dbFindOneDelete, dbFindOneIfExistUpdate} = require('@part/db')
 const {div, divClass} = require('@part/erp')
 const {fsFileCreate} = require('@part/fs')
 const {getFunctionName, getDataToArray, getArrayToOneData} = require('@part/get')
@@ -20,54 +20,151 @@ const {sortDataField} = require('@part/sort')
 router.post('/add', pAdminSuper, aliceAsyncMiddleware(async(req, res) => {
     console.log('add')
     const {erpName, text, nameClass, pageName, pageEnd, _id} = req.body
-    let rankNumber = req.body.rankNumber
-    let pageRank = +req.body.pageRank
+    const rankNumberNew = +req.body.rankNumber
+    console.log('rankNumberNew',rankNumberNew)
+    const pageRank = +req.body.pageRank
     // console.log('add', rankNumber)
     let pageDetail = await dbFindAllArray('erpPage', {pageName: pageName, pageRank: pageRank})
     // console.log('pageDetail',pageDetail)
-    let rankNumberMax = Math.max.apply(Math, pageDetail.map(function(o) { return o.rankNumber; }))
-    console.log('rankNumberMax',rankNumberMax)
-    if(_id) {
+    const rankNumberMax = Math.max.apply(Math, pageDetail.map(function(o) { return o.rankNumber; }))
+    // console.log('rankNumberMax',rankNumberMax)
+    if(_id) { // dealing with rankNumber
         console.log('rankNumber exist')
         // check if rankNumber equal existing
         const erpPageData = await dbFindOneQuery('erpPage', {_id: getPrimaryKey(_id)})
-        let rankCurrent = erpPageData.rankNumber
-        console.log('if ID rank',rankCurrent, rankNumber)
-
-        // if yes
-            // find max rankNumber
-            // increase it by 1
-            // loop backwards until rankNumber and increase that too
-
-    } else {
-        console.log('rankNumber NOT exist')
-        if(rankNumberMax == '-Infinity') {rankNumberMax = 0}
-        rankNumber = rankNumberMax + 1
+        const rankNumberOld = erpPageData.rankNumber
+        console.log('rankNumberOld',rankNumberOld)
+        let intervalStep = 0
+        if(rankNumberOld == rankNumberNew) {
+            console.log('noChange')
+            // direction = 'noChange'
+            // intervalStep = 0
+        } else if(rankNumberNew > rankNumberOld) { // going downwards
+            console.log('downwards', rankNumberNew, rankNumberOld)
+            // direction = 'downard'
+            intervalStep = rankNumberNew - rankNumberOld
+            console.log('intervalStep',intervalStep)
+            for(let i = 0; i < intervalStep; i++) {
+                console.log('downards', [i])
+                let rankSearch = rankNumberOld + 1 + i
+                let rankUpdate = rankNumberOld + i
+                await dbFindOneIfExistUpdate('erpPage', {pageName, pageRank, 'rankNumber': rankSearch}, {'rankNumber': rankUpdate})
+            }
+        } else { // going upwards
+            console.log('upward', rankNumberNew, rankNumberOld)
+            // direction = 'upward'
+            intervalStep = rankNumberOld - rankNumberNew
+            console.log('intervalStep',intervalStep)
+            for(let i = 0; i < intervalStep; i++) {
+                console.log('upwards', [i])
+                let rankSearch = rankNumberNew - 1 - i
+                let rankUpdate = rankNumberNew - i
+                await dbFindOneIfExistUpdate('erpPage', {pageName, pageRank, 'rankNumber': rankSearch}, {'rankNumber': rankUpdate})
+            }
+        }
     }
-    console.log('rankNumber',rankNumber)
+    // console.log('rankNumber',rankNumber)
     let rankOutput = ''
     if(erpName == 'div') {rankOutput = div(text)}
     if(erpName == 'divClass') {rankOutput = divClass(text, nameClass)}
     let rankFunction = {}
-    rankFunction = {pageRank, rankNumber: rankNumber, erpName: erpName, text: text, nameClass: nameClass, pageName: pageName, pageEnd: pageEnd, rankOutput}
-    if(_id) {
-        console.log('id exist')
+    rankFunction = {pageRank, rankNumber: rankNumberNew, erpName: erpName, text: text, nameClass: nameClass, pageName: pageName, pageEnd: pageEnd, rankOutput}
+    if(_id) { // if new part, use id
+        // console.log('id exist')
         await dbFindOneUpdate('erpPage', {_id: getPrimaryKey(_id)}, rankFunction)  
     } else {
-        console.log('id NOT exist')
-        await dbFindOneUpdate('erpPage', {pageName, rankNumber}, rankFunction)  
+        // console.log('id NOT exist')
+        await dbFindOneUpdate('erpPage', {pageName, rankNumber: rankNumberNew}, rankFunction)  
     }
     // get all output and write to file
     // const pageWriteView = await dbFindAllArray('erpPage', {pageName: site, pageRank: 1})
     // console.log('pageWriteView',pageWriteView)
-    const pageWriteView = await dbFindAllArray('erpPage', {pageName: 'site', pageRank: 1})
+    let pageWriteView = await dbFindAllArray('erpPage', {pageName: 'site', pageRank: 1})
     // console.log('pageWriteView',pageWriteView)
     // const data = getDataToArray(pageWriteView, 'rankOutput')
+    pageWriteView = pageWriteView.sort(sortDataField('rankNumber'))
+
     const data = getArrayToOneData(pageWriteView, 'rankOutput')
-    console.log('data',data)
+    // console.log('data',data)
     fsFileCreate(path.join('site', `site.hbs`), data)
     res.redirect('back')
 }))
+// router.post('/add', pAdminSuper, aliceAsyncMiddleware(async(req, res) => {
+//     console.log('add')
+//     const {erpName, text, nameClass, pageName, pageEnd, _id} = req.body
+//     let rankNumber = +req.body.rankNumber
+//     let pageRank = +req.body.pageRank
+//     // console.log('add', rankNumber)
+//     let pageDetail = await dbFindAllArray('erpPage', {pageName: pageName, pageRank: pageRank})
+//     // console.log('pageDetail',pageDetail)
+//     let rankNumberMax = Math.max.apply(Math, pageDetail.map(function(o) { return o.rankNumber; }))
+//     console.log('rankNumberMax',rankNumberMax)
+//     if(_id) { // dealing with rankNumber
+//         console.log('rankNumber exist')
+//         // check if rankNumber equal existing
+//         const erpPageData = await dbFindOneQuery('erpPage', {_id: getPrimaryKey(_id)})
+//         let rankNumberOld = erpPageData.rankNumber
+//         console.log('if ID rank',rankNumberOld, rankNumber)
+//         // var rankNumberChange = 1
+//         if(rankNumberOld != rankNumber) { // rankNumber has been changed, check rankNumbers existing and update existing
+//             console.log('rankNumber changed', rankNumberOld, rankNumber)
+//             if(rankNumber > rankNumberOld) { // rank New bigger rank Old; going downwards
+//                 let rankOld = rankNumberOld
+//                 let rankNew = rankNumber
+//                 if(rankNew > rankNumberMax) {
+//                     rankNew = rankNumberMax
+//                 }
+//                 let rankUpdate = rankNew - 1
+//                 let rankSearch = rankNew
+//                 while(rankSearch != rankNumberOld) {
+//                     await dbFindOneIfExistUpdate('erpPage', {pageName, pageRank, 'rankNumber': rankSearch}, {'rankNumber': rankUpdate})
+//                     rankUpdate--
+//                     rankSearch--
+//                 }
+//             } else { // rank New smaller rank Old; going upwards
+//                 let rankOld = rankNumberOld
+//                 let rankNew = rankNumber
+//                 if(rankNew < 1) { // if rank smaller than 1, set it to 1
+//                     rankNew = 1
+//                 }
+//                 while(rankNew != rankNumberOld) {
+//                     await dbFindOneIfExistUpdate('erpPage', {pageName, pageRank, 'rankNumber': rankNew}, {'rankNumber': rankNew})
+//                     rankNew++
+//                     rankOld++
+//                 }
+//             }
+//         }
+//     } else {
+//         console.log('rankNumber NOT exist')
+//         if(rankNumberMax == '-Infinity') {rankNumberMax = 0}
+//         rankNumber = rankNumberMax + 1
+//     }
+//     console.log('rankNumber',rankNumber)
+//     let rankOutput = ''
+//     if(erpName == 'div') {rankOutput = div(text)}
+//     if(erpName == 'divClass') {rankOutput = divClass(text, nameClass)}
+//     let rankFunction = {}
+//     rankFunction = {pageRank, rankNumber: rankNumber, erpName: erpName, text: text, nameClass: nameClass, pageName: pageName, pageEnd: pageEnd, rankOutput}
+//     if(_id) { // if new part, use id
+//         // console.log('id exist')
+//         await dbFindOneUpdate('erpPage', {_id: getPrimaryKey(_id)}, rankFunction)  
+//     } else {
+//         // console.log('id NOT exist')
+//         await dbFindOneUpdate('erpPage', {pageName, rankNumber}, rankFunction)  
+//     }
+//     // get all output and write to file
+//     // const pageWriteView = await dbFindAllArray('erpPage', {pageName: site, pageRank: 1})
+//     // console.log('pageWriteView',pageWriteView)
+//     let pageWriteView = await dbFindAllArray('erpPage', {pageName: 'site', pageRank: 1})
+//     // console.log('pageWriteView',pageWriteView)
+//     // const data = getDataToArray(pageWriteView, 'rankOutput')
+//     pageWriteView = pageWriteView.sort(sortDataField('rankNumber'))
+
+//     const data = getArrayToOneData(pageWriteView, 'rankOutput')
+//     // console.log('data',data)
+//     fsFileCreate(path.join('site', `site.hbs`), data)
+//     res.redirect('back')
+// }))
 
 router.get('/', pAdminSuper, aliceAsyncMiddleware(async(req, res) => {
     // console.log('homepageview')
--- part/db.js
+++ part/db.js
@@ -7,13 +7,24 @@ module.exports = {
             query // query object, what to find by, the ID of the data. _id get primary key (the object id in mongodb) of the data data
             , {$set : data} // pass in the document to update with. $set update operator. field1: value1. Update the value you want. data is the key field. value is the value of the key field. {field1: value1, fieldx: valuex}
             // , {returnOriginal : true} // false = return the modified object. boolean, default: true
-            , {upsert : true} // false = return the modified object. boolean, default: true
+            , {upsert : true} // true = create data if cant find
             , (err, result) => { // callback function: returns the result from searching the ID
             if(err)
                 console.log(err) 
             }
         )
     } // await dbFindOneUpdate('product', {sku : fieldId}, {pageUrl: pageUrl})     
+    , dbFindOneIfExistUpdate: (collection, query, data) => { // if data exist, update
+        db.getDB().collection(collection).findOneAndUpdate(
+            query
+            , {$set : data}
+            , {upsert : false} // false = dont add if cant find
+            , (err, result) => {
+            if(err)
+                console.log(err) 
+            }
+        )
+    } // await dbFindOneIfExistUpdate('product', {sku : fieldId}, {pageUrl: pageUrl})     
     , dbFindOneQuery: (collection, query) => { // find 1 data: query=search data; field
         const existData = db.getDB().collection(collection).findOne(query) // find first result only
         .then(existData => {
